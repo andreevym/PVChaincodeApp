ab64eaf42f17768138e9dc22113753e7b5b55cad
Kryukov Ruslan <RKryukov@ru.ibm.com>
2016-12-01 14:33:40 +0300

    commit

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ type SimpleChaincode struct {
 }
 
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	chaincodeLogger.Info("##### INIT RUN #####")
+	chaincodeLogger.Errorf("##### INIT RUN #####")
 	var A, B string    // Entities
 	var Aval, Bval int // Asset holdings
 	var err error
@@ -57,7 +57,7 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 		return nil, errors.New("Expecting integer value for asset holding")
 	}
 
-	chaincodeLogger.Info("Current values: A = %d, B = %d\n", Aval, Bval)
+	chaincodeLogger.Errorf("Current values: A = %d, B = %d\n", Aval, Bval)
 
 	// Write the state to the ledger
 	err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
@@ -75,7 +75,7 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 
 // Transaction makes payment of X units from A to B
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	chaincodeLogger.Info("##### INVOKE RUN #####")
+	chaincodeLogger.Errorf("##### INVOKE RUN #####")
 	if function == "delete" {
 		// Deletes an entity from its state
 		return t.delete(stub, args)
@@ -155,7 +155,7 @@ func (t *SimpleChaincode) delete(stub shim.ChaincodeStubInterface, args []string
 
 // Query callback representing the query of a chaincode
 func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	chaincodeLogger.Info("##### QUERY RUN #####")
+	chaincodeLogger.Errorf("##### QUERY RUN #####")
 	if function != "query" {
 		return nil, errors.New("Invalid query function name. Expecting \"query\"")
 	}
@@ -181,13 +181,13 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	}
 
 	jsonResp := "{\"Name\":\"" + A + "\",\"Amount\":\"" + string(Avalbytes) + "\"}"
-	chaincodeLogger.Info("Query Response:%s\n", jsonResp)
+	chaincodeLogger.Errorf("Query Response:%s\n", jsonResp)
 	return Avalbytes, nil
 }
 
 func main() {
 	err := shim.Start(new(SimpleChaincode))
 	if err != nil {
-		chaincodeLogger.Info("Error starting Simple chaincode: %s", err)
+		chaincodeLogger.Errorf("Error starting Simple chaincode: %s", err)
 	}
 }
